c4b700ba8cbeb05efb19687d0b4cd5daef17613a
Smerity <smerity@smerity.com>
2011-09-14 01:22:53 +1000

    Code now generates a PGM file and has incorrect deflection and ray capture code included

--- trunk/src/constants.c
+++ trunk/src/constants.c
@@ -1,6 +1,6 @@
 #include "constants.h"
 
-float kappa_star, kappa_c, gamma_, kappa;
+float kappa_star, kappa_c, gamma_, kappa, source_scale;
 float image_scale_fudge, image_scale_x, image_scale_y;
 float lens_scale_fudge_1, lens_scale_fudge_2, lens_rad_x, lens_rad_y, lens_rad;
 float lens_count;
@@ -9,33 +9,33 @@ void setup_constants(void) {
   /* Two parameters for the lensing system: convergence (kappa) and shear (gamma).
      Convergence can be broken into a smooth component (kappa_c) and a clumpy stellar
      component (kappa_star), such that kappa = kappa_star + kappa_c */
-  float kappa_star = 0.394;           // convergence in stars (sometimes written as sigma instead of kappa)
-  float kappa_c = 0.;                 // convergence in smooth matter
-  float gamma_ = 0.395;                // shear
-  float kappa = kappa_star + kappa_c; // total convergence
+  kappa_star = 0.394;           // convergence in stars (sometimes written as sigma instead of kappa)
+  kappa_c = 0.;                 // convergence in smooth matter
+  gamma_ = 0.395;                // shear
+  kappa = kappa_star + kappa_c; // total convergence
   /* Source plane (where the light rays are collected) is a square with side length source_scale,
      measured in Einstein Radii. */
-  float source_scale = 24.;       // size of the source plane in Einstein Radii
+  source_scale = 24.;       // size of the source plane in Einstein Radii
   /* Size of the image plane (from which the light rays are shot) depends on the desired size of the 
      source plane and the lensing parameters. Note that the image plane is rectangular -- 2d ray 
      positions should be randomly generated in the ranges [-image_scale_x, image_scale_x] and
      [-image_scale_y, image_scale_y].
      Fudge factor values were determined empirically -- leaving them set to the following values 
      will allow direct comparison of output with single core tree code */
-  float image_scale_fudge = 0.1;  // image plane scale fudge factor
-  float image_scale_x = (0.5 + 2.0*image_scale_fudge) * source_scale / (1.0 - kappa - gamma_);
-  float image_scale_y = (0.5 + 2.0*image_scale_fudge) * source_scale / (1.0 - kappa + gamma_);
+  image_scale_fudge = 0.1;  // image plane scale fudge factor
+  image_scale_x = (0.5 + 2.0*image_scale_fudge) * source_scale / (1.0 - kappa - gamma_);
+  image_scale_y = (0.5 + 2.0*image_scale_fudge) * source_scale / (1.0 - kappa + gamma_);
   /* Radius of the lens plane (where the microlenses are distributed) also depends on the desired size
      of the source plane and the lensing parameters. We use a circular lens plane of radius lens_rad.
      Some notes apply about the fudge factors. */
-  float lens_scale_fudge_1 = 5.0; // lens plane scale fudge factor 1
-  float lens_scale_fudge_2 = 2.0; // lens plane scale fudge factor 2
-  float lens_rad_x = (0.5*source_scale + lens_scale_fudge_1) / (1.0 - kappa + gamma_);
-  float lens_rad_y = (0.5*source_scale + lens_scale_fudge_1) / (1.0 - kappa - gamma_);
-  float lens_rad = sqrt(lens_rad_x*lens_rad_x + lens_rad_y*lens_rad_y) + lens_scale_fudge_2;
+  lens_scale_fudge_1 = 5.0; // lens plane scale fudge factor 1
+  lens_scale_fudge_2 = 2.0; // lens plane scale fudge factor 2
+  lens_rad_x = (0.5*source_scale + lens_scale_fudge_1) / (1.0 - kappa + gamma_);
+  lens_rad_y = (0.5*source_scale + lens_scale_fudge_1) / (1.0 - kappa - gamma_);
+  lens_rad = sqrt(lens_rad_x*lens_rad_x + lens_rad_y*lens_rad_y) + lens_scale_fudge_2;
   /* lens_count is the required number of microlenses. These should be uniformly randomly distributed 
      within a circle of radius lens_rad. */  
-  size_t lens_count = (size_t)(kappa_star * lens_rad*lens_rad / 1 + 0.5);
+  lens_count = (size_t)(kappa_star * lens_rad*lens_rad / 1 + 0.5);
   // TODO: Eventually this should be the avg. lens mass
   // 1 is the lens mass
 }
--- trunk/src/main.c
+++ trunk/src/main.c
@@ -5,15 +5,21 @@
 #include "constants.h"
 #include "util.h"
 
+float *lens_x;
+float *lens_y;
+float *lens_mass;
+size_t nobjects;
+
 /* read_lenses • Loads a lens file of format {x, y, (optional)mass} and allocate the correct sized array for the attributes */
-void read_lenses(float *lens_x, float *lens_y, float *lens_mass, const char *filename) {
-  size_t i, len = 0, nobjects = 0;
+void read_lenses(const char *filename) {
+  size_t i, len = 0;
   char c, *tmp, *line = NULL;
   FILE *fp;
 
   fprintf(stderr, "Reading in lenses...\n");
   if (!(fp = fopen(filename, "r"))) error("Can't open lens file...");
 
+  nobjects = 0;
   // Count the number of lenses we must allocate for (one per line)
   while ((c = getc(fp)) != EOF) {
     if (c == '\n') ++nobjects;
@@ -45,16 +51,91 @@ void read_lenses(float *lens_x, float *lens_y, float *lens_mass, const char *fil
   free(line);
 }
 
+/* deflect • Deflect a given light ray according to the gravitational bodies along its path */
+void deflect(float *x, float *y) {
+  size_t i;
+  float dist;
+  // Start X / Y
+  float start_x = *x, start_y = *y;
+  *x -= gamma_;
+  *y += gamma_;
+  *x -= kappa_c * start_x;
+  *y -= kappa_c * start_y;
+  for(i = 0; i < nobjects; ++i) {
+    dist = sqrt(pow(start_x - lens_x[i], 2) + pow(start_y - lens_y[i], 2));
+    *x -= lens_mass[i] * (start_x - lens_x[i]) / dist;
+    *y -= lens_mass[i] * (start_y - lens_y[i]) / dist;
+  }
+}
+
+/* write_pgm • Output the results as a PGM (portable gray map) image for review */
+void write_pgm(int *results, int pixel_x, int pixel_y, int highest) {
+  FILE *fout;
+  fprintf(stderr, "Writing resulting image...\n");
+  if (!(fout = fopen("img.pgm", "w"))) error("Can't open results file...");
+  // Writing the PGM format which starts with P2
+  fprintf(fout, "P2\n");
+  // Followed by pixel width, height and the value considered white
+  fprintf(fout, "%d %d\n", pixel_x, pixel_y);
+  fprintf(fout, "%d\n", highest);
+  // Print each value in a row of WIDTH length
+  int px, py;
+  for(py = 0; py < pixel_y; ++py) {
+    for(px = 0; px < pixel_x; ++px) {
+      fprintf(fout, "%d ", results[py * pixel_x + px]);
+    }
+    fprintf(fout, "\n");
+  }
+  if (fclose(fout) != 0) error("Can't close results file...");
+}
+
 int main(int argc, const char *argv[])
 {
-  float *lens_x;
-  float *lens_y;
-  float *lens_mass;
+  float x, y, dx, dy, increment_x, increment_y;
+  int pixel_x = 512, pixel_y = 512, it;
 
+  // Load relevant settings and data
   if (argc < 2) error("Requires argument with lens positions and optional mass");
-
   setup_constants();
-  read_lenses(lens_x, lens_y, lens_mass, argv[1]);
+  read_lenses(argv[1]);
+
+  fprintf(stderr, "X %f and Y %f\n", image_scale_x, image_scale_y);
+  increment_x = (image_scale_x * 2) / pixel_x;
+  increment_y = (image_scale_y * 2) / pixel_y;
+  fprintf(stderr, "Increments for X %f and Y %f\n", increment_x, increment_y);
+
+  int *results = (int *)calloc(pixel_x * pixel_y, sizeof(float));
+  int highest = 0;
+  if (!results) error("calloc failed in allocating the result array");
+  for(it = 0; it < 1; ++it) {
+    for(y = -image_scale_y; y < image_scale_y; y += increment_y) {
+      for(x = -image_scale_x; x < image_scale_x; x += increment_x) {
+        // Noise should be up to one increment
+        //float noise = (((float)rand())/RAND_MAX * increment_x) - (increment_x/2);
+        dx = x;
+        //noise = (((float)rand())/RAND_MAX * increment_y) - (increment_y/2);
+        dy = y;
+        deflect(&dx, &dy);
+        // Source plan (where collected) is -source_scale/2 to source_scale/2
+        if ((dx >= -source_scale/2) && (dx <= source_scale/2) &&
+            (dy >= -source_scale/2) && (dy <= source_scale/2)) {
+          // Work out the nearest pixel to put this in to
+          //result[py * pixel_x + px]
+          // Add to remove the negative part of source_scale and then source_scale / pixels
+          int px = (dx + source_scale/2) / (source_scale/pixel_x);
+          int py = (dy + source_scale/2) / (source_scale/pixel_y);
+          results[py * pixel_x + px] += 1;
+          if (results[py * pixel_x + px] > highest) highest = results[py * pixel_x + px];
+        }
+      }
+      // Provide an update counter in percent for our current progress
+      fprintf(stderr, "\r%1.0f%% ", 100*(y+image_scale_y)/(image_scale_y*2));
+    }
+    fprintf(stderr, "\nIteration Number %d\n", it);
+  }
+  fprintf(stderr, "\n");
+
+  write_pgm(results, pixel_x, pixel_y, highest);
 
   return 0;
 }
