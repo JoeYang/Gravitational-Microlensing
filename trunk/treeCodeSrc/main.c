0cdb5cbff5012b96208bc49bd6caf83a9b543899
Katrina Nally <kjnallster@gmail.com>
2011-09-29 01:31:57 +1000

    merged tree code with other CPU code

---
+++ trunk/treeCodeSrc/main.c
@@ -0,0 +1,121 @@
+#include "global.h"
+#include "constants.h"
+#include "util.h"
+#include "tree_code.h"
+
+float *lens_x;
+float *lens_y;
+float *lens_mass;
+size_t nobjects;
+cell *root;
+cell *curr;
+cell *cells;
+
+/* deflect • Deflect a given light ray according to the gravitational bodies along its path */
+void deflect(float *x, float *y) {
+  size_t i;
+  float dist;
+  // Start X / Y
+  float start_x = *x, start_y = *y;
+  *x = (1-gamma_)*start_x - kappa_c*start_x;
+  *y = (1+gamma_)*start_y - kappa_c*start_y;
+  for(i = 0; i < nobjects; ++i) {
+    dist = pow(start_x - lens_x[i], 2) + pow(start_y - lens_y[i], 2);
+    *x -= lens_mass[i] * (start_x - lens_x[i]) / dist;
+    *y -= lens_mass[i] * (start_y - lens_y[i]) / dist;
+  }
+}
+
+/* Set up the root node */ 
+void setup_root(float top_x, float top_y, float bottom_x, float bottom_y, int ncells) {
+  int i;
+  curr = (cell *)salloc(sizeof(cell));
+  curr->index = 0;
+  curr->top_left[0] = top_x;
+  curr->top_left[1] = top_y;
+  curr->bottom_right[0] = bottom_x;
+  curr->bottom_right[1] = bottom_y;
+  curr->mass = 0;
+  curr->cm_x = 0;
+  curr->cm_y = 0;
+  
+  for (i = 0; i < ncells; ++i) {
+    curr->subcells[i] = 0;
+  }
+  
+  root = curr;
+}
+
+int main(int argc, const char *argv[])
+{
+  float increment_x, increment_y;
+  int pixel_x = 512, pixel_y = 512, it;
+  unsigned int rpp = 500;
+  int ncells = 4;
+  float accuracy = 0.6;
+
+  // Load relevant settings and data
+  if (argc < 2) error("Requires argument with lens positions and optional mass");
+  setup_constants();
+  read_lenses(argv[1]);
+  
+  setup_root(-lens_rad, lens_rad, lens_rad, -lens_rad, ncells);
+  
+  build_tree(ncells, root);
+  calculate_cm(root, ncells);
+
+  printf("All cells\n");
+  printf("index, centre mass x, centre mass y, total mass\n"); 
+  remove_empty_cells(root, ncells);
+  print_tree(root, ncells);
+
+  fprintf(stderr, "X %f and Y %f\n", image_scale_x, image_scale_y);
+  increment_x = (image_scale_x * 2) / pixel_x;
+  increment_y = (image_scale_y * 2) / pixel_y;
+  fprintf(stderr, "Increments for X %f and Y %f\n", increment_x, increment_y);
+
+  unsigned int *results = (int *)calloc(pixel_x * pixel_y, sizeof(unsigned int));
+  if (!results) error("calloc failed in allocating the result array");
+  int highest = 0;
+
+  // Fire off the light rays and record their end locations
+  int complete_iterations = 0;
+  #pragma omp parallel for
+  for(it = 0; it < rpp; ++it) {
+    float x, y, dx, dy;
+    for(y = -image_scale_y; y < image_scale_y; y += increment_y) {
+      for(x = -image_scale_x; x < image_scale_x; x += increment_x) {
+        // Noise is uniformly distributed -- i.e. it's not really noise
+        float noise_x = it * increment_x / rpp;
+        float noise_y = it * increment_y / rpp;
+        dx = x + noise_x;
+        dy = y + noise_y;
+        //get_included_bodies(root, accuracy, dx, dy);
+        deflect(&dx, &dy);
+        // Source plan (where collected) is -source_scale/2 to source_scale/2
+        if ((dx > -source_scale/2) && (dx < source_scale/2) &&
+            (dy > -source_scale/2) && (dy < source_scale/2)) {
+          // Work out the nearest pixel to put this in to
+          // Add to remove the negative part of source_scale and then source_scale / pixels
+          int px = (dx + source_scale/2) / (source_scale/pixel_x);
+          int py = source_scale/ (source_scale/pixel_y) - (dy + source_scale/2) / (source_scale/pixel_y);
+          results[py * pixel_x + px] += 1;
+          if (results[py * pixel_x + px] > highest) highest = results[py * pixel_x + px];
+        }
+      }
+    }
+    fprintf(stderr, "\r%4d/%4d Complete\r", ++complete_iterations, rpp);
+  }
+  assert(highest > 0 && "No pixels were written on the output map");
+  write_pgm(results, pixel_x, pixel_y, highest);
+
+  // Free the memory allocated during processing
+  free(lens_x);
+  free(lens_y);
+  free(lens_mass);
+  free(results);
+  //free_tree(root);
+	free(curr);
+
+  return 0;
+}
--- trunk/treeCodeSrc/tree_code.h
+++ trunk/treeCodeSrc/tree_code.h
@@ -6,32 +6,30 @@
 #include <math.h>
 
 /* data structure to store a lens' position and mass */
-typedef struct lens lens;
-typedef struct cell cell;
 
-struct lens {
+typedef struct lens {
 	float pos_x;
 	float pos_y;
 	float mass;
-};
+} lens;
 
 /* data structure to store information about a cell */
-struct cell {
+typedef struct cell {
 	int index; // position where cell is stored in array, 'cells'
 	float top_left[2], bottom_right[2]; // x, y co-ordinates that define the dimensions of the cell
 	float cm_x; // centre of mass, x
 	float cm_y; // centre of mass, y
 	float mass; // total mass the cell's lenses
-	cell *subcells[4]; // indexes of the subcells
-	lens clens; // lens inside the cell. If the lens has subcells this doesn't exist.
+	struct cell *subcells[4]; // indexes of the subcells
+	struct lens clens; // lens inside the cell. If the lens has subcells this doesn't exist.
 	/* float quadrupole_moment; */
 	/* float higher_multipole_moment; */
-};
+} cell;
 
-void setup_root(float, float, float, float, int);
-void build_tree(int);
+//void setup_root(float, float, float, float, int);
+void build_tree(int, cell *);
 void calculate_cm(cell *, int);
-void *salloc(size_t);
+//void *salloc(size_t);
 void print_tree(cell *, int);
 void free_tree(cell *, int);
 void remove_empty_cells(cell *, int);
