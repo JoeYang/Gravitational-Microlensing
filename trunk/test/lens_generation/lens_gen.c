10c4921db16c067b24fadfb0e246b28ea55ac29e
Merity; Stephen <Stephen Merity smerity@it.usyd.edu.au>
2011-10-05 21:10:18 +1100

    Ensuring the lenses are within a circle of lens_rad, cleaning up the Makefile and adding error messages and other helper functions (salloc) where relevant

--- trunk/test/lens_generation/lens_gen.c
+++ trunk/test/lens_generation/lens_gen.c
@@ -2,17 +2,22 @@
 #include <math.h>
 
 void lens_generate(float *new_lens_x, float *new_lens_y, int nobjects){
-	
-	setup_constants();
-		
-	int it;	
-	srand(time(NULL));
-	
-	for(it = 0; it < nobjects; ++it){
-		float x, y;
-        x = lens_rad_x*rand() / RAND_MAX;
-        y = lens_rad_y*rand() / RAND_MAX;
-        new_lens_x[it] = x;
-        new_lens_y[it] = y; 
-	}
+  setup_constants();
+
+  int it = 0;
+  srand(time(NULL));
+
+  while(it < nobjects){
+    float x, y;
+    x = 2*lens_rad_x*rand() / RAND_MAX;
+    y = 2*lens_rad_y*rand() / RAND_MAX;
+    x -= lens_rad_x;
+    y -= lens_rad_y;
+    // Ensure the point is within the cirlce of radius lens_rad
+    if (pow(x,2) + pow(y,2) <= pow(lens_rad,2)) {
+      new_lens_x[it] = x;
+      new_lens_y[it] = y;
+      ++it;
+    }
+  }
 }
