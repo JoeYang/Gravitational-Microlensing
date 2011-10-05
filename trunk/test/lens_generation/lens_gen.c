3539c3e6152195e459e5ad3fa80852618a6165ab
joeyang <xyan2284@gmail.com>
2011-10-05 16:21:25 +1100

    correction

--- trunk/test/lens_generation/lens_gen.c
+++ trunk/test/lens_generation/lens_gen.c
@@ -1,29 +1,18 @@
 #include "constants.h"
 #include <math.h>
 
-void lens_generate(float *new_lens_x, float *new_lens_y, int rpp, int pixel){
+void lens_generate(float *new_lens_x, float *new_lens_y, int nobjects){
 	
 	setup_constants();
-	float increment_x = (image_scale_x * 2) / pixel;
-  	float increment_y = (image_scale_y * 2) / pixel;
-	
-	int size = pixel*pixel*rpp;
-	int i = 0;
+		
 	int it;	
 	srand(time(NULL));
 	
-	for(it = 0; it < rpp; ++it){
+	for(it = 0; it < nobjects; ++it){
 		float x, y;
-		for(y = -image_scale_y; y < image_scale_y; y += increment_y) {
-      		for(x = -image_scale_x; x < image_scale_x; x += increment_x) {
-		        // Noise is uniformly distributed -- i.e. it's not really noise
-		        float noise_x = increment_x*rand() / RAND_MAX;
-		        float noise_y = increment_x*rand() / RAND_MAX;
-		        new_lens_x[i] = x + noise_x;
-		        new_lens_y[i] = y + noise_y;
-  			   	i++;
-
-            }
-    	} 
+        x = lens_rad_x*rand() / RAND_MAX;
+        y = lens_rad_y*rand() / RAND_MAX;
+        new_lens_x[it] = x;
+        new_lens_y[it] = y; 
 	}
 }
--- trunk/test/lens_generation/main.c
+++ trunk/test/lens_generation/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "lens_gen.c"
 
-#define pixel	20
-#define rpp		20
+#define size	20
 
 int main(){
-	int size = pixel*pixel*rpp;
+	
 	float *lens_gen_x = (float*)malloc(size*sizeof(float));
 	float *lens_gen_y = (float*)malloc(size*sizeof(float));
 	
-	lens_generate(lens_gen_x, lens_gen_y, rpp, pixel);	
+	lens_generate(lens_gen_x, lens_gen_y, size);	
 
 	int it;
 	for(it = 0; it< size; it++){
