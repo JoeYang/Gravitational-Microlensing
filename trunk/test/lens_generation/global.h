fb1c602fea8dcca1359a754eaff63a0664cd184a
joeyang <xyan2284@gmail.com>
2011-10-06 00:48:00 +1100

    fix commit

---
+++ trunk/test/lens_generation/global.h
@@ -0,0 +1,10 @@
+// List of headers common across all files
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern float *lens_x;
+extern float *lens_y;
+extern float *lens_mass;
+extern size_t nobjects;
--- trunk/test/lens_generation/util.h
+++ trunk/test/lens_generation/util.h
@@ -1,1 +1,12 @@
-../../src/util.h
+#include "global.h"
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+void *salloc(size_t bytes);
+void error(const char *msg);
+void nextline(FILE *input, char **buf, size_t *len);
+void read_lenses(const char *filename);
+void write_pgm(unsigned int *results, int pixel_x, int pixel_y, int highest);
+
+#endif
