e5b0d2badb783910aa0c7be851d7060279e48039
Andrew Stevens <andrew.mj.stevens@gmail.com>
2011-10-29 12:25:00 +1100

    Sorry about the delay, I thought I pushed the program to github but obviously I was wrong, my bad but here is the working treecode for cuda

--- trunk/cudaTreeSrc/tree_struct.c
+++ trunk/cudaTreeSrc/tree_struct.c
@@ -15,17 +15,63 @@ void make_root(cell **root){
    */
   int i;
   (*root) = (cell *)salloc(sizeof(cell));
-  (*root)->width = 1.0;
+  //(*root)->width = 1.0;
+  (*root)->length = 1.0;
   (*root)->index = 0;
   (*root)->height = 0;
   (*root)->top_left[0] = -1.0*(float)(lens_rad);
   (*root)->top_left[1] = (float)(lens_rad);
   (*root)->bot_right[0] = (float)(lens_rad);
   (*root)->bot_right[1] = -1.0*(float)(lens_rad);
+  (*root)->width = (*root)->bot_right[0] - (*root)->top_left[0];
   for(i=0; i < QUAD_IDX; i++){
     (*root)->lenses[i] = NULL;
     (*root)->desc[i] = NULL;
   }
+}
+
+/* method for getting the number of lenses to include
+*  if the ratio is greater than one we resolve it to one
+*
+*/
+void get_lens_count(cell ** tree, float delta, float ray_x, float ray_y, int * lens_count){
+  //static int val = 0;                 /* static variable to contain the number of lenses to include */
+  int j, has_cells=0, has_lenses=0;
+
+  /* finding the values of the current cell to decide if it is to be included in the calculation*/
+  float cell_x = (float)(*tree)->center_mass_x/(float)(*tree)->total_mass;
+  float cell_y = (float)(*tree)->center_mass_y/(float)(*tree)->total_mass;
+  float dist = sqrt(pow(ray_x - cell_x,2) + pow(ray_y - cell_y, 2));
+  float ratio = ((*tree)->width)/dist;
+
+  // checking if the current cell has any cells or lenses
+  for(j=0; j<QUAD_IDX; j++){
+    if((*tree)->lenses[j]) has_lenses++;
+    if((*tree)->desc[j]) has_cells++;
+  }
+
+  if(delta == 0.0 && has_lenses>0){
+    (*lens_count) += has_lenses;
+    int i;
+    for(i=0; i<QUAD_IDX; i++){
+      if((*tree)->desc[i]) get_lens_count(&((*tree)->desc[i]), delta, ray_x, ray_y, lens_count);
+    }
+  }
+  else if(has_lenses==1 && has_cells==0){
+    (*lens_count)++;
+    return;
+  }
+  else if(ratio<delta){
+    (*lens_count)++;
+    return;
+  }
+  else{
+    int i;
+    for(i=0; i<QUAD_IDX; i++){
+      if((*tree)->desc[i]) get_lens_count(&((*tree)->desc[i]), delta, ray_x, ray_y, lens_count);
+    }
+  }
+  //return (*lens_count);
 }
 
 /* 
@@ -42,13 +88,13 @@ int get_lens_pos(cell *tree, lens *lens){
     return 0;
   }
   //this check is to see if the lens is in the top left quadrant
-  else if(lens->x >= tree->top_left[0] && lens->x < (tree->top_left[0] + tree->bot_right[0])/2.0
-     && lens->y > (tree->top_left[1] + tree->bot_right[1])/2.0 && lens->y <= tree->top_left[1]){
+  else if(lens->x >= tree->top_left[0] && lens->x <= (tree->top_left[0] + tree->bot_right[0])/2.0
+     && lens->y >= (tree->top_left[1] + tree->bot_right[1])/2.0 && lens->y <= tree->top_left[1]){
     return 1;
   }
   //check if the lens is in the bottom left quadrant
-  else if(lens->x >= tree->top_left[0] && lens->x < (tree->top_left[0] + tree->bot_right[0])/2.0
-           && lens->y >= tree->bot_right[1] && lens->y < (tree->top_left[1] + tree->bot_right[1])/2.0){
+  else if(lens->x >= tree->top_left[0] && lens->x <= (tree->top_left[0] + tree->bot_right[0])/2.0
+           && lens->y >= tree->bot_right[1] && lens->y <= (tree->top_left[1] + tree->bot_right[1])/2.0){
     return 2;
   }
   //check if the lens is in the bottom right quadrant (maybe change this later to an else)
@@ -57,9 +103,9 @@ int get_lens_pos(cell *tree, lens *lens){
     return 3;
   }
   else{
- //   printf("The x and y values from the lens: x: %.8f, y: %.8f\n", lens->x, lens->y);
- //   printf("The x and y values from the top left of tree: x: %.8f, y: %.8f\n", tree->top_left[0], tree->top_left[1]);
- //   printf("The x and y values from the bottom right of tree: x: %.8f, y: %.8f\n", tree->bot_right[0], tree->bot_right[1]);
+   // printf("The x and y values from the lens: x: %.8f, y: %.8f\n", lens->x, lens->y);
+   // printf("The x and y values from the top left of tree: x: %.8f, y: %.8f\n", tree->top_left[0], tree->top_left[1]);
+   // printf("The x and y values from the bottom right of tree: x: %.8f, y: %.8f\n", tree->bot_right[0], tree->bot_right[1]);
     error("could not find the lens position\n");
   }
 
@@ -74,9 +120,10 @@ cell * create_cell(cell ** tree, lens *nlens, lens *olens, int pos){
   c->total_mass = olens->m + nlens->m;      /* total mass of the cell*/
   c->center_mass_x = (olens->m * olens->x) + (nlens->m * nlens->x); /* numerator for calculation of center of mass in x */
   c->center_mass_y = (olens->m * olens->y) + (nlens->m * nlens->y); /* numerator for calculation of center of mass in y */
-  c->width = (*tree)->width/2.0;                                    /* width of cell in the graph*/
+  //c->width = (*tree)->width/2.0;                                    /* width of cell in the graph*/
+  c->length = (*tree)->length/2.0;                                    /* width of cell in the graph*/
   c->index = QUAD_IDX*((*tree)->index) + pos + 1;               /* way to calculate index of node in tree*/
-  recip = (int)(1.0/(float)(c->width));
+  recip = (int)(1.0/(float)(c->length));
   while(recip > 1){
     recip /= 2;
     count++;
@@ -121,6 +168,7 @@ cell * create_cell(cell ** tree, lens *nlens, lens *olens, int pos){
     c->bot_right[0] = (*tree)->bot_right[0];
     c->bot_right[1] = (*tree)->bot_right[1];
   }
+  c->width = c->bot_right[0] - c->top_left[0];
 
   return c;
 }
@@ -149,38 +197,33 @@ void add_lens(cell ** tree, lens **olens, lens **nlens, int pos){
 
 /*method to create Barnes-Hut tree*/
 void make_tree(cell ** tree, lens *lens){
-  //printf("==================================================================================================\n");
   int i, lens_pos;
   struct lens *temp = (struct lens *)salloc(sizeof(struct lens));                /*allocating memory to temp lens*/
+  /* adding the properties of *lens to *temp */
   temp->x = lens->x;
   temp->y = lens->y;
   temp->m = lens->m;
   lens_pos = get_lens_pos(*tree, lens);
-  //printf("the position of the current lens: %d\n", lens_pos);
-  //printf("The values of the current lens: x: %f, y: %f\n", lens->x, lens->y);
   (*tree)->total_mass += lens->m;
   (*tree)->center_mass_x += lens->m*lens->x;
   (*tree)->center_mass_y += lens->m*lens->y;
-  //printf("value of the temp node: x: %f, y: %f, m: %f\n", temp->x, temp->y, temp->m);
-  //printf("The positions of the tree node: top_left x: %f, top_left y: %f\n", (*tree)->top_left[0], (*tree)->top_left[1]);
-  //printf("The positions of the tree node: bot_right x: %f, bot_right y: %f\n", (*tree)->bot_right[0], (*tree)->bot_right[1]);
 
 	if(!((*tree)->desc[0]) && !((*tree)->desc[1]) && !((*tree)->desc[2]) && !((*tree)->desc[3])){
-    //printf("current tree node has no descendents\n");
     
     if(!((*tree)->lenses[lens_pos])){
       (*tree)->lenses[lens_pos] = temp;
     }
+    else if(((*tree)->lenses[lens_pos]->x == temp->x) && ((*tree)->lenses[lens_pos]->y == temp->y)){
+      (*tree)->lenses[lens_pos]->m += temp->m;
+    }
     else{
       /* we have a lens here and so we need to create a new descendent node*/
-      //printf("The values of the lens in the tree: x: %f, y: %f, m: %f\n", (*tree)->lenses[lens_pos]->x, (*tree)->lenses[lens_pos]->y, (*tree)->lenses[lens_pos]->m);
       add_lens(tree, &((*tree)->lenses[lens_pos]), &temp, lens_pos);
       (*tree)->lenses[lens_pos] = NULL;
     }
       
   }
   else{     /* here if there are already cells */
-    //printf("There are already cells in the tree\n");
 
     /* if there is a cell in this position*/
     if(!((*tree)->desc[lens_pos]) && !((*tree)->lenses[lens_pos])){
@@ -188,19 +231,21 @@ void make_tree(cell ** tree, lens *lens){
     }
     else if((*tree)->desc[lens_pos] && !((*tree)->lenses[lens_pos])){
       free(temp);
-      //printf("The top_left of the descendent: %d x: %.8f, y: %.8f\n", lens_pos, (*tree)->desc[lens_pos]->top_left[0], (*tree)->desc[lens_pos]->top_left[1]);
-     // printf("The bottom_right of the descendent: %d x: %.8f, y: %.8f\n", lens_pos, (*tree)->desc[lens_pos]->bot_right[0], (*tree)->desc[lens_pos]->bot_right[1]);
       make_tree(&((*tree)->desc[lens_pos]), lens);
     }
     else if(!((*tree)->desc[lens_pos]) && (*tree)->lenses[lens_pos]){
-      //printf("The values of the lens in the tree: x: %f, y: %f, m: %f\n", (*tree)->lenses[lens_pos]->x, (*tree)->lenses[lens_pos]->y, (*tree)->lenses[lens_pos]->m);
-      add_lens(tree,&((*tree)->lenses[lens_pos]), &temp, lens_pos);
-      (*tree)->lenses[lens_pos] = NULL;
+      if(((*tree)->lenses[lens_pos]->x == temp->x) && ((*tree)->lenses[lens_pos]->y == temp->y)){
+        (*tree)->lenses[lens_pos]->m += temp->m;
+        free(temp);
+      }
+      else{
+        add_lens(tree,&((*tree)->lenses[lens_pos]), &temp, lens_pos);
+        (*tree)->lenses[lens_pos] = NULL;
+      }
     }
     else{
       error("Error occurred while constructing tree");
     }
-
   }
 }
 
@@ -222,7 +267,7 @@ void print_tree(cell ** tree){
       printf("lens y-coord: %.8f\n", (*tree)->lenses[i]->y);
     }
   }
-  printf("values for any lenses the cell has: \n");
+  printf("printing the values : \n");
   for(i=0; i<QUAD_IDX; i++){
     if((*tree)->desc[i]) print_tree(&((*tree)->desc[i]));
   }
